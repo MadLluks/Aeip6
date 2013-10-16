7b91442039cd6c226ff48111d6324e857ab31b2e
Alexandre Cantais <alexandre.cantais@gmail.com>
2013-10-16 11:11:08 +0200

    FirstCommit

---
+++ src/application/aeip6/AccueilActivity.java
@@ -0,0 +1,23 @@
+package application.aeip6;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AccueilActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.accueil, menu);
+        return true;
+    }
+    
+}
