14a5df8790f4c4f0c4b3b66d03d513ebe419ecfd
mHelba <william.chasson@gmail.com>
2013-10-17 16:05:33 +0200

    testMaj

--- gen/application/aeip6/R.java
+++ gen/application/aeip6/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
+        public static final int action_settings=0x7f080003;
+        public static final int boutonMembres=0x7f080002;
+        public static final int boutonPolys=0x7f080000;
+        public static final int pointCentral=0x7f080001;
     }
     public static final class layout {
         public static final int activity_accueil=0x7f030000;
@@ -37,9 +38,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int button_membres=0x7f050004;
-        public static final int button_polys=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int bouton_membres=0x7f050003;
+        public static final int bouton_polys=0x7f050002;
     }
     public static final class style {
         /** 
