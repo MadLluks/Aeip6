d4b4567a53b7d5aa9fa438f1cd05715fc59d338d
Alexandre Cantais <alexandre.cantais@gmail.com>
2013-10-20 13:20:30 +0200

    MaJ

--- src/application/aeip6/AccueilActivity.java
+++ src/application/aeip6/AccueilActivity.java
@@ -1,27 +1,49 @@
 package application.aeip6;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class AccueilActivity extends Activity {
+	public final static String NUM_ETUDIANT = "3152609";
 	private Button poly = null;
-	
-    @Override
+	private SharedPreferences preferences;
+	private SharedPreferences.Editor editor;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        
+        this.setPreferences(PreferenceManager.getDefaultSharedPreferences(this));
+        this.setEditor(this.getPreferences().edit());
+        
+        Log.d("debug", "ceci est un test");
         
         poly = (Button)findViewById(R.id.poly);
         
         poly.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              Intent intent = new Intent(AccueilActivity.this, LoginActivityPolys.class);
-              startActivity(intent);
+            	SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(AccueilActivity.this);
+            	Intent intent;
+            	int num = preferences.getInt(NUM_ETUDIANT, 0);
+            	Log.d("debug", String.valueOf(num));
+            	if(preferences.getInt(NUM_ETUDIANT, 0) == 0){
+            		intent = new Intent(AccueilActivity.this, LoginActivityPolys.class);
+                	startActivity(intent);
+            	}
+            	else{
+            		intent = new Intent(AccueilActivity.this, AccueilActivity.class);
+            		startActivity(intent);
+            	}
               
             }
           });
@@ -35,5 +57,24 @@ public class AccueilActivity extends Activity {
         getMenuInflater().inflate(R.menu.accueil, menu);
         return true;
     }
+    
+    public SharedPreferences getPreferences() {
+		return preferences;
+	}
+
+
+	public void setPreferences(SharedPreferences preferences) {
+		this.preferences = preferences;
+	}
+
+
+	public SharedPreferences.Editor getEditor() {
+		return editor;
+	}
+
+
+	public void setEditor(SharedPreferences.Editor editor) {
+		this.editor = editor;
+	}
     
 }
